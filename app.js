a6d943f3b400efc34fcec482d4df826bfbca5dae
yusufadell <youseefadel777@gmail.com>
2022-05-21 23:34:07 +0200

    fix: Create mongodb connection from atlas database server

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://yusufadell:246813579/*@cluster0.i7woh.mongodb.net/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
